1580527128e8982ffb667d5b86fcc731b25502ff
Silvia Trejo <silvia.trejo@fdmgroup.com>
2023-04-18 17:07:50 -0400

    formating

---
+++ IntroJAVA/src/chapter1/ProgrammingStyle.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+public class ProgrammingStyle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Java");
+
+	}
+
+}
+
+//it is important to use good proggramming style and documentation guidelines
+//i.e use end of line brace style.
