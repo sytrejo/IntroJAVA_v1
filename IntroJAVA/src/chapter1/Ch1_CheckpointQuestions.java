de0a97def0476bcb5afd77f6d06ccb7136c179a5
Silvia Trejo <silvia.trejo@fdmgroup.com>
2023-04-17 22:33:10 -0400

    working on ch1

--- IntroJAVA/src/chapter1/Ch1_CheckpointQuestions.java
+++ IntroJAVA/src/chapter1/Ch1_CheckpointQuestions.java
@@ -13,6 +13,14 @@ public class Ch1_CheckpointQuestions {
 //	A: five major hardware components: CPU, MEmory< Storage Devices, Input/Output Devices, and Communication Devices
 	
 // 3. What does the acronym CPU stand for? What unit is used to measure CPU speed?
+// A: CPU Stands for Central Processing Unit.
+//	It is the brain of the computer. The unit of measurement of clock speed is the hertz(Hz), with 1 hertz
+//	equaling 1 pulse per second. The clock speed of a computer is usually stated in megahertz(MHz). 
+//	(1 MHz is 1 million Hz)
+	
+
+//	4. What is a bit? What is a byte?
+//	a bit is a binary digit ) or 1. A byte is a sequence of 8 bits.
 	
 	
 	
