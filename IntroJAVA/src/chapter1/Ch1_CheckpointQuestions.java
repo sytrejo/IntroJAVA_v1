a841d3646131103b5a5dd0fd025ef3d1f73aa0ca
Silvia Trejo <silvia.trejo@fdmgroup.com>
2023-04-18 12:18:56 -0400

    answering more questions

--- IntroJAVA/src/chapter1/Ch1_CheckpointQuestions.java
+++ IntroJAVA/src/chapter1/Ch1_CheckpointQuestions.java
@@ -46,6 +46,12 @@ public class Ch1_CheckpointQuestions {
 //	The program written in a programming language is called a source program.
 	
 	
+/** 10. What is an interpreter? What is a compiler?
+ * 	A: An interpreter is a software that reads one statement from the source code, translates it to the machine code or 
+ * virtual machine code, and then executes it right away. A compiler is a software that translates a program in high-level
+ * language into machine language code
+ */
+	
 	
 	
 
