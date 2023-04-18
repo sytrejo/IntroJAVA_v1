15c916c381abb569fd747d4d7a96190b4789c7a2
Silvia Trejo <silvia.trejo@fdmgroup.com>
2023-04-17 22:27:45 -0400

    first questions

---
+++ IntroJAVA/src/chapter1/Ch1_CheckpointQuestions.java
@@ -0,0 +1,21 @@
+package chapter1;
+
+public class Ch1_CheckpointQuestions {
+	
+//	1. What are hardware and software
+	
+//	A: A computer is an electronic device that stores and processes data. A computer includes both hardware and software.
+//	In general, hardware is the physical aspect of the computer that can be seen, and software is the invisible instructions that control
+//	the hardware and make it work. The hardware of a computer consists of a CPU, cache, memory, hard disk, floppy disk, monitor
+//	printer, and communication devices.
+	
+// 2. List five major hardware components of a computer?
+//	A: five major hardware components: CPU, MEmory< Storage Devices, Input/Output Devices, and Communication Devices
+	
+// 3. What does the acronym CPU stand for? What unit is used to measure CPU speed?
+	
+	
+	
+	
+
+}
