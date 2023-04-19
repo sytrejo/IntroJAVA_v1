8a69f0ccfe400ca1dacb446d3c7f4c278abbdc30
sytrejo <s.y.trejo26@gmail.com>
2023-04-19 11:31:46 -0400

    working on ch2

---
+++ IntroJAVA/src/chapter2_CheckPointQuestions/S2_4.java
@@ -0,0 +1,14 @@
+package chapter2_CheckPointQuestions;
+
+public class S2_4 {
+	/*
+	 * 1. Which of the following identifiers are valid? Which are Java keywords?
+	 * 
+	 * Valid identifiers: miles, Test, $4, apps, x, y, radius
+	 * Invalid identifiers: a++, a--, 4#R, #44, class, public, int
+	 * Keywords: class, public, int
+	 * 
+	 * 
+	 */
+
+}
---
+++ IntroJAVA/src/chapter2_CheckPointQuestions/S2_5.java
@@ -0,0 +1,15 @@
+package chapter2_CheckPointQuestions;
+
+public class S2_5 {
+	
+	//What is wrong with the following code:
+
+	public static void main(String[] args) {
+		int i = k + 2;
+		System.out.println(i);
+		
+		//k is undefined
+
+	}
+
+}
