19216f724a4c4fd5281ae198155c0f4d79199dd8
sytrejo <s.y.trejo26@gmail.com>
2023-04-18 23:03:43 -0400

    1.8

---
+++ IntroJAVA/src/chapter1_EndChapterProblems/P01_08.java
@@ -0,0 +1,14 @@
+package chapter1_EndChapterProblems;
+
+public class P01_08 {
+	
+	public static void main(String[] args) {
+		
+		//Display area
+		System.out.println(5.5 * 5.5 * 3.14);
+		
+		//Display perimeter
+		System.out.println(2 * 5.5 * 3.14);
+	}
+
+}
