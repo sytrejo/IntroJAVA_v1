2a2fdd117dc8b5b84f526922b37095127b390c5f
sytrejo <s.y.trejo26@gmail.com>
2023-04-19 16:24:02 -0400

    2.18

---
+++ IntroJAVA/src/chapter2_EndChapterProblems/Ex02_18.java
@@ -0,0 +1,25 @@
+package chapter2_EndChapterProblems;
+
+import java.util.*;
+
+public class Ex02_18 {
+	
+	/*
+	 * Write a program that displays a table
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		
+	    System.out.println("a      b      pow(a, b)");
+	    //math.pow(1,2) is 1 squared
+	    System.out.println("1      2      " + (int)Math.pow(1, 2));
+	    System.out.println("2      3      " + (int)Math.pow(2, 3));
+	    System.out.println("3      4      " + (int)Math.pow(3, 4));
+	    System.out.println("4      5      " + (int)Math.pow(4, 5));
+	    System.out.println("5      6      " + (int)Math.pow(5, 6));
+		
+		
+	}
+
+}
