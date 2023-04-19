ca0dacc150f5fc03693e199bd6c68beaea07ad47
sytrejo <s.y.trejo26@gmail.com>
2023-04-19 11:26:35 -0400

    working on ch2 check point questions

---
+++ IntroJAVA/src/chapter2_CheckPointQuestions/S2_2.java
@@ -0,0 +1,18 @@
+package chapter2_CheckPointQuestions;
+
+public class S2_2 {
+
+	public static void main(String[] args) {
+		 double i = 50.0;
+		 double k = i + 50.0;
+		 double j = k + 1;
+		 
+		 System.out.println("j is " + j + " and k is " + k);
+		 
+		 //Notes
+		 //The main method should have the static keyword
+		 //string in the main method should always be capitalized
+		 //A string should not be broken into two lines. i.e the sys.out statement.
+	}
+
+}
---
+++ IntroJAVA/src/chapter2_CheckPointQuestions/S2_3.java
@@ -0,0 +1,25 @@
+package chapter2_CheckPointQuestions;
+
+public class S2_3 {
+	
+	/*
+	 * 1. How do you write a statement to let the user enter a double value from the keyboard? 
+	 * What happens if you entered 5a when executing the following code?
+	 * double radius = input.nextDouble();
+	 * 
+	 * To let the user enter input use:
+	 * Scanner input = new Scanner(System.in);
+	 * double value = input.nextDouble();
+	 * 
+	 * if you attempt to run the code a runtime error will occur if you enter 5a when executing the code.
+	 * 
+	 * 2. Are there any performance differences between the following two import statements?
+	 * import java.util.Scanner;
+	 * import java.util.*
+	 * 
+	 * No
+	 * 
+	 */
+
+
+}
