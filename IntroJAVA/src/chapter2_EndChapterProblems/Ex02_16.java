4048c6127c6dbdcaf15784e4cc267c82adb5427c
sytrejo <s.y.trejo26@gmail.com>
2023-04-19 16:16:52 -0400

    2.16

---
+++ IntroJAVA/src/chapter2_EndChapterProblems/Ex02_16.java
@@ -0,0 +1,28 @@
+package chapter2_EndChapterProblems;
+
+import java.util.*;
+
+public class Ex02_16 {
+	
+	/*
+	 * Write a program that prompts the user to enter the side of a hexagon and displays its area.
+	 */
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		//Enter the side of the hexagon
+		System.out.print("Enter the length of the side: ");
+		double side = input.nextDouble();
+		
+		
+		//Compute the area
+		double area = 3 * Math.pow(3,  0.5) * side * side /2;
+		
+		System.out.println("The area of the hexagon is " + area);
+		
+
+	}
+
+}
