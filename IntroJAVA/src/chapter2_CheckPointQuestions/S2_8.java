414026eeadb92a2907ab24cfd9989564ce6c472b
sytrejo <s.y.trejo26@gmail.com>
2023-04-19 11:53:25 -0400

    adding section 2.8 and some 2.9

---
+++ IntroJAVA/src/chapter2_CheckPointQuestions/S2_8.java
@@ -0,0 +1,18 @@
+package chapter2_CheckPointQuestions;
+
+public class S2_8 {
+	
+	/*
+	 * What are the naming conventions for class names, method names, constants, and variables?
+	 * Which of the following items can be a constant, a method, a variable, or a class according to the
+	 * Java naming convention?
+	 * 
+	 * Class names: capitalize the first letter in each name
+	 * Variables and method names: lowercase the first word, Capitalize the first letter in all subsequent words
+	 * Constants: capitalize all letters
+	 * Costant: MAX_VALUE
+	 * method or variable: read, readDouble
+	 * Class name: Test
+	 */
+
+}
---
+++ IntroJAVA/src/chapter2_CheckPointQuestions/S2_9.java
@@ -0,0 +1,43 @@
+package chapter2_CheckPointQuestions;
+
+public class S2_9 {
+	/*
+	 * Find the largest and smallest byte, short, int, long float, and double. 
+	 * Which of these data types requires the least amount of memory? 
+	 * 		For byte, from -128 to 127, inclusive.
+			For short, from -32768 to 32767, inclusive.
+			For int, from -2147483648 to 2147483647, inclusive.
+			For long, from -9223372036854775808 to 9223372036854775807.
+			For float, the smallest positive float is 1.40129846432481707e-45 and the largest float is 3.40282346638528860e+38.
+			For double, the smallest positive double is 4.94065645841246544e-324 and the largest double is 1.79769313486231570e+308d.
+
+	 */
+
+	public static void main(String[] args) {
+		
+		//Show the result of the following remainders
+		System.out.println("******2.9.2******");
+		System.out.println(56%6);
+		System.out.println(78%-4);
+		System.out.println(-34%5);
+		System.out.println(-34%-5);
+		System.out.println(5%1);
+		System.out.println(1%5);
+		
+		//if today is Tuesday, what will be the day in 100 days?
+		System.out.println("************2.9.2.2**********");
+		
+		System.out.println((2+100)%7); //4 so the day is Thursday
+		
+		//What is the result of 25/4? How would you rewrite the expression if you wished the result to be a floating-point number?
+		System.out.println("***********2.9.2.3***************");
+		System.out.println(25/4); //6
+		
+		//if you want the quotient to be a floating-point number, rewrite it as:
+		System.out.println(25.0/4.0);
+		System.out.println(25.0/4);
+		System.out.println(25/4.0);
+
+	}
+
+}
