9dc2bf0fde5253bd543146fc0692c0afffe3a0d8
sytrejo <s.y.trejo26@gmail.com>
2023-04-26 13:09:08 -0400

    updating

---
+++ IntroJAVA/src/ch3_CheckPointQuestions/S3_5.java
@@ -0,0 +1,25 @@
+package ch3_CheckPointQuestions;
+
+public class S3_5 {
+
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 2;
+		int z;
+		
+		x = 3;
+		y = 4;
+		
+		if(x > 2) {
+			if(y > 2) {
+				z = x + y;
+				System.out.println("z is " + z);
+			}
+		} else {
+			System.out.println("x is " + x);
+		}
+			
+
+	}
+
+}
