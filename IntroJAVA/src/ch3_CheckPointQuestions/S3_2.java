9de4310d71f1213baae589673ccc58bf6cff685a
sytrejo <s.y.trejo26@gmail.com>
2023-04-25 21:50:07 -0400

    ch3 section 2

---
+++ IntroJAVA/src/ch3_CheckPointQuestions/S3_2.java
@@ -0,0 +1,18 @@
+package ch3_CheckPointQuestions;
+
+public class S3_2 {
+	/**
+	 * 1. List six relational operators
+	 * 
+	 * A: < , <=, == , !=, > >=
+	 * 
+	 * 2. 
+	 *
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
