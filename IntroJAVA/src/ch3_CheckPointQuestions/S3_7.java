35748f3a9b6671ace7790d5640875de08b9ff6fb
sytrejo <s.y.trejo26@gmail.com>
2023-04-27 21:10:20 -0400

    updating

--- IntroJAVA/src/ch3_CheckPointQuestions/S3_7.java
+++ IntroJAVA/src/ch3_CheckPointQuestions/S3_7.java
@@ -13,7 +13,19 @@ public class S3_7 {
 	 * 		f. 0.234
 	 * 
 	 * Answer:  b, e, and f. Math.random will return a value from 0 to .99 unless you add arithmatic.
-	 * 	
+	 * 
+	 * 2a. How do you generate a random integer i such that 0<= i < 20
+	 * 		Answer: (int)(Math.random()* 20)
+	 * 
+	 * 2b. How do you generate a random integer i such that 10 <= i < 20
+	 * 		Answer: 10 + (int)(Math.random() * 10)
+	 * 
+	 * 2c. How do you generate a random integer i such that 10 <= i <= 50
+	 * 		Answer: 10 + (int)(Math.random() * 41)
+	 * 
+	 * 2d. Write an expression that returns 0 or 1 randomly
+	 * 		Answer: (int)(Math.random()* 2)
+	 * 
 	 */
 
 	public static void main(String[] args) {
