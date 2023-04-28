d1d322263854e47dc250d70851b40b03efdb8640
sytrejo <s.y.trejo26@gmail.com>
2023-04-27 21:05:58 -0400

    3.7

---
+++ IntroJAVA/src/ch3_CheckPointQuestions/S3_7.java
@@ -0,0 +1,24 @@
+package ch3_CheckPointQuestions;
+
+public class S3_7 {
+	
+	/*
+	 * 1. Which of the following is a possible output from invoking Math.random()?
+	 * 
+	 * 		a. 323.4
+	 * 		b. 0.5
+	 * 		c. 34
+	 * 		d. 1.0
+	 * 		e. 0.0
+	 * 		f. 0.234
+	 * 
+	 * Answer:  b, e, and f. Math.random will return a value from 0 to .99 unless you add arithmatic.
+	 * 	
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
