5f12b2d3046ed287330ada8f1b99d8be94c44cdd
sytrejo <s.y.trejo26@gmail.com>
2023-05-02 19:15:44 -0400

    updates

---
+++ IntroJAVA/src/ch3_CheckPointQuestions/S3_14.java
@@ -0,0 +1,50 @@
+package ch3_CheckPointQuestions;
+
+public class S3_14 {
+	
+//	1. Rewrite the following if statements using the conditional operator.
+//	if (ages >= 16)
+//	  ticketPrice = 20;
+//	else 
+//	  ticketPrice = 10;
+//	ticketPrice = (ages >= 16) ? 20 : 10;
+	
+//	2. Rewrite the following conditional expressions using if-else statements.
+//	(a) score = (x > 10) ? 3 * scale : 4 * scale; 
+//	(b) tax = (income > 10000) ? income * 0.2 : income * 0.17 + 1000; 
+//	(c) System.out.println((number % 3 == 0) ? i : j);
+//	(a) 
+//	if (x > 10) 
+//	  score = 3 * scale;
+//	else
+//	  score = 4 * scale;
+//
+//	(b)
+//	if (income > 10000) 
+//	  tax = income * 0.2;
+//	else
+//	  tax = income * 0.17 + 1000;
+//
+//	(c)
+//	if (number % 3 == 0) 
+//	      System.out.println(i);
+//	else
+//	      System.out.println(j);
+	
+//	3. Write conditional expression that returns -1 or 1 randomly.
+//	
+//	(int)(Math.random() * 2) == 0 ? -1 : 1;
+	
+//	4. List the precedence order of the Boolean operators. Evaluate the following expressions:
+//		true || true && false
+//		true && true || false
+
+//	5. All the binary operators except assignment operators (including augmented assignment operators) are left associative.
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
