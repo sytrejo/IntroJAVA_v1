67830691770b0342ffdc71e6efc2c166e32b72ea
sytrejo <s.y.trejo26@gmail.com>
2023-05-08 18:30:38 -0400

    updates

--- IntroJAVA/src/chapter3_EndChapterProblems/Ex03_30.java
+++ IntroJAVA/src/chapter3_EndChapterProblems/Ex03_30.java
@@ -1,9 +1,45 @@
 package chapter3_EndChapterProblems;
 
+import java.util.*;
+
 public class Ex03_30 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+	    Scanner input = new Scanner(System.in);
+	    System.out.print("Enter the time zone offset to GMT: ");
+	    long timeZoneOffset = input.nextInt();
+	    
+	    // Obtain the total milliseconds since the midnight, Jan 1, 1970
+	    long totalMilliseconds = System.currentTimeMillis();
+
+	    // Obtain the total seconds since the midnight, Jan 1, 1970
+	    long totalSeconds = totalMilliseconds / 1000;
+
+	    // Compute the current second in the minute in the hour
+	    long currentSecond = totalSeconds % 60;
+
+	    // Obtain the total minutes
+	    long totalMinutes = totalSeconds / 60;
+
+	    // Compute the current minute in the hour
+	    long currentMinute = totalMinutes % 60;
+
+	    // Obtain the total hours
+	    long totalHours = totalMinutes / 60;
+
+	    // Compute the current hour
+	    long currentHour = (totalHours + timeZoneOffset) % 24;
+
+	    // Display results
+	    System.out.print("Current time is " + (currentHour % 12) + ":"
+	      + currentMinute + ":" + currentSecond);
+	    
+	    if (currentHour < 12)
+	      System.out.println(" AM");
+	    else
+	      System.out.println(" PM");  
 
 	}
 
